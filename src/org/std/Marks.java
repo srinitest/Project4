0a568e21949d32f5f993779871aa076138f2775f
Balaji250692 <yuvaraghavan87@gmail.com>
2020-12-19 20:22:36 +0530

    order placed

--- src/org/std/Marks.java
+++ src/org/std/Marks.java
@@ -1,28 +1,20 @@
 package org.std;
 
 public class Marks {
-public static void main(String[] args) {
-	int id=5473769;
-	System.out.println("Student ID:"+id);
-	String name="Vijay";
-	System.out.println("Student Name:"+name);
-	byte m1=91;
-	System.out.println("Mark 1:"+m1);
-	byte m2=92;
-	System.out.println("Mark 2:"+m2);
-	byte m3=93;
-	System.out.println("Mark 3:"+m3);
-	byte m4=94;
-	System.out.println("Mark 4:"+m4);
-	byte m5=95;
-	System.out.println("Mark 5:"+m5);
-	System.out.println("hi");
-	System.out.println("hi");
-	System.out.println("hi");
-	System.out.println("hi");
-	System.out.println("hi");
-	System.out.println("hi");
-	System.out.println("hi");
-	
-}
+	public static void main(String[] args) {
+		System.out.println("asdfgh");
+
+		System.out.println("asdfgh");
+		System.out.println("asdfgh");
+		System.out.println("asdfgh");
+		
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+		System.out.println("hi");
+
+	}
 }
